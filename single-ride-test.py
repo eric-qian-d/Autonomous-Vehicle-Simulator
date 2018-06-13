5ab5480a8fcb28ffae4f0a9e1b00f72f95be6abb
eqian7 <eric.qian.d@gmail.com>
2018-06-13 15:55:19 +0800

    semi-working copy

--- single-ride-test.py
+++ single-ride-test.py
@@ -11,7 +11,7 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 	the total amount of time waited
 	'''
 
-	def update_in_vehicle(): #need to considre order of dropoff for rideshares; FIX!
+	def update_in_vehicle(R, R_IV, R_S, V_I, V_D): #need to consider order of dropoff for rideshares; FIX!
 		if len(R_IV) == 0: return
 		v_done, r_done = [], []
 		for vehicle in V_D:
@@ -31,12 +31,14 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 		for vehicle in v_done:
 			V_D.remove(vehicle)
 			V_I.add(vehicle)
+			vehicle.state = 'idle'
 
 		for passenger in r_done:
 			R_IV.remove(passenger)
 			R_S.add(passenger)
+			passenger.state = 'served'
 
-	def update_assigned():
+	def update_assigned(R, R_A, R_IV, V_P, V_D):
 		if len(R_A) == 0: return
 		v_done, r_done = [], []
 		for vehicle in V_P:
@@ -56,10 +58,12 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 		for vehicle in v_done:
 			V_P.remove(vehicle)
 			V_D.add(vehicle)
+			vehicle.state = 'drop off'
 
 		for passenger in r_done:
 			R_A.remove(passenger)
 			R_IV.add(passenger)
+			passenger.state = 'in vehicle'
 
 	def update_unassigned():
 		if len(R_U) < 1: return
@@ -67,8 +71,6 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 
 	def solve_R_lessthan_V(R, R_A, R_prime, V, V_P, V_prime):
 		#Initialize variables
-
-		# x_init, x_prime_init = [[0 for i in range(len(R))] for j in range(len(V))], [[0 for i in range(len(R))] for j in range(len(V))]
 		d, y, d11, d12, d21, rideshare_pen = [[0 for j in range(num_vehicles)] for i in range(num_passengers)], [[0 for j in range(num_vehicles)] for i in range(num_passengers)], [[0 for j in range(num_vehicles)] for i in range(num_passengers)], [[0 for j in range(num_vehicles)] for i in range(num_passengers)], [[0 for j in range(num_vehicles)] for i in range(num_passengers)], [[0 for j in range(num_vehicles)] for i in range(num_passengers)]
 		p, q = [0] * len(V), [0] * len(V)
 		b, w = [0] * len(R), [0] * len(R)
@@ -96,8 +98,6 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 					d12[i][j] = point_dist(considered_passenger.o, current_passenger.d) + point_dist(current_passenger.d, considered_passenger.d) - point_dist(considered_passenger.o, considered_passenger.d)
 					d21[i][j] = point_dist((vehicle.x, vehicle.y), current_passenger.d) + point_dist(considered_passenger.o, considered_passenger.d) + point_dist(considered_passenger.d, current_passenger.d) - dist_to_d(current_passenger, vehicle)
 					rideshare_pen[i][j] = [psi1 * d11[i][j] + psi2 * d12[i][j], 0] if psi1 * d11[i][j] + psi2 * d12[i][j] < psi1 * d21[i][j] else [psi1 * d21[i][j], 1]
-
-
 				else:
 					d11[i][j] = 0
 					d12[i][j] = 0
@@ -116,7 +116,6 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 		x_prime = pulp.LpVariable.dicts('shared_ride', ((i, j) for i in range(len(R)) for j in range(len(V))), lowBound = 0, upBound = 1, cat = 'Discrete')
 
 		#Set objective function
-
 		model += (pulp.lpSum([pulp.lpSum([[x[(i,j)] * (d[i][j] + phi * p[j])]
 		+ [x_prime[(i,j)] * rideshare_pen[i][j][0]]
 		+ [delta * q[j] * (x[(i,j)] + x_prime[(i,j)]) * (1 - y[i][j])]
@@ -125,17 +124,18 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 		#Set constraints
 		for j in range(num_vehicles):
 			label = 'rideshare_pass_constraint_%d' % j
-
-			print(label)
-
 			condition = p[j] >= pulp.lpSum([[x_prime[(i,j)]] for i in range(len(R_prime))]) #passenger 1 before 2 ride-share
 			model += condition, label
 
+			label_single = 'one_max_single_%d' % j
+			label_shared = 'one_max_shared_%d' % j
+			condition_single = pulp.lpSum([[x[(i, j)]] for i in range(len(R_prime))]) <= 1 #cap the number of single rides for each vehicle
+			condition_shared = pulp.lpSum([[x_prime[(i, j)]] for i in range(len(R_prime))]) <= 1 #cap the number of ride shares for each vehicle
+			model += condition_single, label_single
+			model += condition_shared, label_shared
+
 		for i in range(num_passengers):
 			label = 'all_assigned_%d' % i
-
-			print(label)
-
 			condition = pulp.lpSum([[x[(i,j)]] + [x_prime[(i,j)]] for j in range(len(V_prime))]) == 1 #every passenger is assigned
 			model += condition, label
 
@@ -143,10 +143,6 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 			for j in range(num_vehicles):
 				label_single = 'nonneg_single_' + str(i) + '_' + str(j)
 				label_shared = 'nonneg_shared_' + str(i) + '_' + str(j)
-
-				print(label_single)
-				print(label_shared)
-
 				condition_single = x[(i,j)] >= 0 #nonnegative single rides
 				condition_shared = x_prime[(i,j)] >= 0 #nonnegative ride shares
 				model += condition_single, label_single
@@ -154,19 +150,12 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 
 		for i in range(len(R_A)):
 			label = 'stay_assigned_%d' % i
-
-			print(label)
-
 			condition = pulp.lpSum([x[(i,j)]] + [x_prime[(i,j)]]) #prevents assigned -> unassigned
 			model += condition, label
 
 			for j in range(len(V_P)):
 				label_single = 'reassign_single_' + str(i) + '_' + str(j)
 				label_shared = 'reassign_shared_' + str(i) + '_' + str(j)
-
-				print(label_single)
-				print(label_shared)
-
 				condition_single = b[i] * (y[i][j] - x[(i,j)]) <= 0  #prevents more than 1 reassignment
 				condition_shared = b[i] * (y[i][j] - x_prime[(i,j)]) <= 0  #prevents more than 1 reassignment
 				model += condition_single, label_single
@@ -174,8 +163,18 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 
 
 		LpSolverDefault.msg = 1
-		print('here we are')
 		model.solve()
+		for var in model.variables():
+			if var.varValue != 0:
+				if 'single_ride' in var.name:
+					first_start = var.name.indexOf('(') + 1 #13
+					first_end = var.name.indexOf(',') - 1
+					second_start = var.name.indexOf(',') + 2
+					second_end = var.name.indexOf(')') - 1
+				else:
+
+
+			print(var.name + ' = ' + str(var.varValue))
 
 	#set constants
 	phi = drop_off_pen
@@ -184,16 +183,18 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 	psi1 = pass1_pen
 	psi2 = pass2_pen
 	T = time_length
-	t = 0
+	t = time_interval
 	t_int = time_interval
 	distance_travel = t_int * vehicle_speed
 
 	#initiate passenger sets
 	R, R_U, R_A, R_IV, R_S, R_prime = [], set(), set(), set(), set(), set()
 	for passenger in range(num_passengers):
-		p = Passenger(passenger, x_max, y_max)
+		p = Passenger(passenger, x_max, y_max, T)
 		R.append(p)
-		R_prime.add(p)
+		if p.appear >= time_interval:
+			R_prime.add(p)
+			R_U.add(p)
 
 	#initiate vehicle sets
 	V, V_I, V_P, V_D = [], set(), set(), set()
@@ -203,13 +204,17 @@ def simulate_rideshare(num_passengers, num_vehicles, vehicle_speed, x_max, y_max
 		V.append(v)
 
 	#loop
-	while t < T:
+	while t <= T:
 		solve_R_lessthan_V(R, R_A, R_prime, V, V_P, V_prime)
 		t += t_int
+		for p in R:
+			if p.appear >= t:
+				R_prime.add(p)
+				R_U.add(p)
 
 
 class Passenger:
-	def __init__(self, num, x_max, y_max):
+	def __init__(self, num, x_max, y_max, time_horizon):
 		self.num = num
 		self.x = random.random() * x_max
 		self.y = random.random() * y_max
@@ -219,7 +224,7 @@ class Passenger:
 		self.vehicle = None 
 		self.reassigned = 0
 		self.wait = 0
-
+		self.appear = random.random() * time_horizon
 
 	def v(self):
 		return self.vehicle
@@ -250,17 +255,18 @@ def point_dist(p1, p2):
 	y_d = p1[1] - p2[1]
 	return math.sqrt(x_d**2 + y_d**2)
 
-#Where you can choose inputs:
-number_of_passengers = 1
-number_of_vehicles = 1
+#Where you can choose inputs - NOTE VEHICLES MUST BE GREATER THAN PASSENGERS:
+number_of_passengers = 5
+number_of_vehicles = 5
 vehicle_speed = 2
 x_size = 40
 y_size = 40
-run_horizon = 10
+run_horizon = 1
 update_interval = 1
 dropoff_reasssignment_penalty = 1
 reassignment_penalty = 1
 waiting_penalty = 1
 pass1_distance_pen = 1
 pass2_distance_pen = 1
+
 simulate_rideshare(number_of_passengers, number_of_vehicles, vehicle_speed, x_size, y_size, run_horizon, update_interval, dropoff_reasssignment_penalty, reassignment_penalty, waiting_penalty, pass1_distance_pen, pass2_distance_pen)
